931c1f5a3768050f2b511185b9939ce94a54ddef
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-05-04 17:29:20 +0100

    :sparkles: feat: add elements (View and Text) to the application's main screen

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,7 +1,19 @@
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet
+} from "react-native";
+
 export default function Home() {
-    return (
-      <>
-      
-      </>
-    );
-  };
+  return (
+    <>
+      <View>
+        <Text>Earth</Text>
+      </View>
+      <View>
+        <Text>Latest news and some tips bellow</Text>
+      </View>
+    </>
+  );
+};

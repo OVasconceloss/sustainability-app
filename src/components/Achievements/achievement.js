cb68a1bbfee2d35e8049cc4a92717d1045e16e78
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-05-06 18:23:33 +0100

    :sparkles: feat: create achievement component (for profile)

---
+++ src/components/Achievements/achievement.js
@@ -0,0 +1,12 @@
+import {
+    View,
+    Text
+} from "react-native";
+
+export default function AchievementBox() {
+    return (
+        <View style={{width: 100, backgroundColor: '#000'}}>
+            <Text>Conquista</Text>
+        </View>
+    )
+}

f5c75e75e54892df6e755bb7fa6aba7472e853e5
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-05-06 18:23:14 +0100

    :sparkles: feat: add links to menu buttons (for navigation)

--- src/components/Footer/footerMenu.js
+++ src/components/Footer/footerMenu.js
@@ -4,16 +4,19 @@ import {
     TouchableOpacity 
  } from "react-native";
 import footerMenuStyle from "./footerMenuStyle";
+import { useNavigation } from "@react-navigation/native";
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function FooterMenu() {
+    const navigation = useNavigation();
+
     return (
         <View style={[footerMenuStyle.footerMenu, footerMenuStyle.shadow]}>
-            <TouchableOpacity style={footerMenuStyle.menuItem}>
+            <TouchableOpacity style={footerMenuStyle.menuItem} onPress={() => navigation.navigate("Home")}>
                 <Icon name="home" size={24} color="#16A34A" />
                 <Text style={footerMenuStyle.menuText}>Home</Text>
             </TouchableOpacity>
-            <TouchableOpacity style={footerMenuStyle.menuItem}>
+            <TouchableOpacity style={footerMenuStyle.menuItem} onPress={() => navigation.navigate("Profile")}>
                 <Icon name="user" size={24} color="#16A34A" />
                 <Text style={footerMenuStyle.menuText}>Profile</Text>
             </TouchableOpacity>

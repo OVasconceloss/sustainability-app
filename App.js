9223b57a9555387eb8ba333205c04774d34f4528
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-05-06 15:12:40 +0100

    :sparkles: feat: import and create stack screen for home page

--- App.js
+++ App.js
@@ -1,9 +1,19 @@
 import HomeScreen from "./src/screens/Home/homeScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <>
-    <HomeScreen />
-    </>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };

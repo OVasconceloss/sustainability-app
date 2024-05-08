d39f7f908e82e4d614116442abb268b1a62dad2b
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-05-08 16:01:55 +0100

    :sparkles: feat: add elements to settings screen

--- src/screens/Settings/settingsScreen.js
+++ src/screens/Settings/settingsScreen.js
@@ -9,32 +9,29 @@ import FooterMenu from "../../components/Footer/footerMenu";
 import DropdownChoice from "../../components/DropDownChoices/dropdownChoice";
 
 export default function SettingsScreen() {
-    
   const languageOptions = [
     { label: 'English', value: 'en' },
     { label: 'Português', value: 'pt' },
   ];
-
-
-    return(
-        <>
-          {/* HEADER */}
-          <View style={settingsStyle.headerSettings}>
-            <View style={[settingsStyle.headerSettingsTitleBox]}>
-              <Text style={settingsStyle.headerSettingsTitle}>Settings</Text>
+  
+  return(
+    <>
+      {/* HEADER */}
+      <View style={settingsStyle.headerSettings}>
+        <View style={[settingsStyle.headerSettingsTitleBox]}>
+          <Text style={settingsStyle.headerSettingsTitle}>Settings</Text>
+        </View>
+      </View>
+      {/* MAIN */}
+      <View>
+          <View>{/*uma div para caixa de botões*/}
+            <View>{/*uma div para botão*/}
+              <DropdownChoice label="Language" options={languageOptions} />
             </View>
           </View>
-          {/* MAIN */}
-          <View>
-            <View>{/*uma div para caixa de botões*/}
-                <View>{/*uma div para botão*/}
-                    <DropdownChoice label="Language" options={languageOptions} />
-                </View>
-            </View>
-          </View>
-        
-            {/* FOOTER */}
-            <FooterMenu />
-        </>
+      </View>
+      {/* FOOTER */}
+      <FooterMenu />
+      </>
    );
 };

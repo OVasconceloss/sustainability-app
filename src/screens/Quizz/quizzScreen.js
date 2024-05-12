095e7bf42d8586a7449505a98a53bc7d88388738
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-05-12 16:20:16 +0100

    :sparkles: feat: update the elements and their styles (quizz screen)

--- src/screens/Quizz/quizzScreen.js
+++ src/screens/Quizz/quizzScreen.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { 
     View,
-    Text
+    Text,
+    TouchableOpacity
 } from "react-native";
 import quizzStyle from "../../styles/quizzStyle";
+import FooterMenu from "../../components/FooterMenu/footerMenu";
 
 export default function QuizzScreen() {
     return (
@@ -11,23 +13,27 @@ export default function QuizzScreen() {
         <View style={quizzStyle.headerQuizz}>
             <Text style={quizzStyle.headerQuizzTitle}>Quizzes</Text>
         </View>
-        <View>
-            <Text>Rules</Text>
-            <View>
-                <View>
-                    <Text>Right Answers:</Text>
-                    <Text>1</Text>
+        <View style={quizzStyle.mainQuizz}>
+            <Text style={quizzStyle.mainQuizzTitle}>Rules</Text>
+            <View style={quizzStyle.mainRulesBox}>
+                <View style={quizzStyle.ruleBox}>
+                    <Text style={quizzStyle.ruleBoxText}>Right Answers: </Text>
+                    <Text style={quizzStyle.ruleBoxValue}>1</Text>
                 </View>
-                <View>
-                    <Text>Wrong Answers:</Text>
-                    <Text>1</Text>
+                <View style={quizzStyle.ruleBox}>
+                    <Text style={quizzStyle.ruleBoxText}>Wrong Answers: </Text>
+                    <Text style={[quizzStyle.ruleBoxValue, quizzStyle.ruleBoxValueWrong]}>1</Text>
                 </View>
             </View>
         </View>
-        <View>
-            <Text>Get all five questions right in order to complete the quiz.</Text>
-            <Text>Come back tomorrow for a new test!</Text>
+        <View style={quizzStyle.informationTextBox}>
+            <Text style={quizzStyle.informationText}>Get all five questions right in order to complete the quiz.</Text>
+            <Text style={quizzStyle.informationText}>Come back tomorrow for a new test!</Text>
+            <TouchableOpacity style={quizzStyle.startButton}>
+                <Text style={quizzStyle.startButtonText}>Start Quizz</Text>
+            </TouchableOpacity>
         </View>
+        <FooterMenu />
         </>
     );
 };

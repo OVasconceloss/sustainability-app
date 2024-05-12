09ecd83fee8c51d58960cba1d5dbcfab00abd1c4
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-05-12 15:56:19 +0100

    :sparkles: feat: create the main elements of the quiz screen

--- src/screens/Quizz/quizzScreen.js
+++ src/screens/Quizz/quizzScreen.js
@@ -3,11 +3,31 @@ import {
     View,
     Text
 } from "react-native";
+import quizzStyle from "../../styles/quizzStyle";
 
 export default function QuizzScreen() {
     return (
         <>
-        
+        <View style={quizzStyle.headerQuizz}>
+            <Text style={quizzStyle.headerQuizzTitle}>Quizzes</Text>
+        </View>
+        <View>
+            <Text>Rules</Text>
+            <View>
+                <View>
+                    <Text>Right Answers:</Text>
+                    <Text>1</Text>
+                </View>
+                <View>
+                    <Text>Wrong Answers:</Text>
+                    <Text>1</Text>
+                </View>
+            </View>
+        </View>
+        <View>
+            <Text>Get all five questions right in order to complete the quiz.</Text>
+            <Text>Come back tomorrow for a new test!</Text>
+        </View>
         </>
     );
 };
